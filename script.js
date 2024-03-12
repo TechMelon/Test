207fdacd1608f651c6f37d1becdb9a22920c80cb
sneha <snehabhardwaj035@gmail.com>
2024-03-12 13:34:54 +0530

    git cmds

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("Hello")
+
+// For leader
+// git init -> git ko is folder mai intialise 
+// git add . -> add all files
+// git commit -m "msg" -> Let others know what you have done 
+// git branch -M branch_Name -> to creat a branch
+// git remote add origin link -> add repo link
+// gt push origin barnch_name -> to push the code 
+
+
+
+
+
+// For others
+// git clone link
+// after added code
+// git add . 
+// git commit -m "msg"
+//git branch -M branch_Name 
+// git push origin branch_Name
+
+
+//To check your branch use the cmd -> git branch
+// for main branch  -> you have to commit you code 1st and then pull the code ad then push 
+
+//git add .
+// git commit -m "msg"
+// git pull orgin main
+// git push origin main
